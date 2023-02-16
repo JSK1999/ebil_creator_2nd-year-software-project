85643ad2785133daac14a1f5e09551794f05fe4e
Nadun Atugoda <atugodarrrmawntb.20@uom.lk>
2023-02-17 01:00:59 +0530

    frontend login page admin

--- Software_project_web_application/backend/index.js
+++ Software_project_web_application/backend/index.js
@@ -1,67 +1,63 @@
 import express from "express"
 import mysql from "mysql2"
- const app = express ()
-   
- const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"203010@#NAmaths",
-    database : "projectwork"
- })
+const app = express()
 
- //if there is any authentication problem 
+const db = mysql.createConnection({
+   host: "localhost",
+   user: "root",
+   password: "203010@#NAmaths",
+   database: "projectwork"
+})
+
+//if there is any authentication problem 
 // ALTER USER 'root'@'3306' IDENTIFIED WITH mysql_native_password BY '203010@#NAmaths';
 
-app.use(express.json)
+app.use(express.json())
 
 
 
 
- app.get("/",(req,res)=>{
-    res.json ("hello this is the backend")
+app.get("/", (req, res) => {
+   res.json("hello this is the backend")
 
- })
- app.get("/salesrep",(req,res)=>{
-    const q ="SELECT * FROM sales_rep"
-    db.query(q,(err,data)=>{
-        if (err)return res.json(err);
-        return res.json(data);
-   
-    })
- })
- app.post ("/salesrep",(req,res)=>{
-    const q = "INSERT INTO sales_rep ('RID','NIC','registrationdate','fullname','password','email','phoneNo','type','address') VALUES (?)"
-    const values = [
-req.body.RID,
-req.body.NIC,
-req.body.registrationdate,
-req.body.fullname,
-req.body.password,
-req.body.email,
-req.body.phoneNo,
-req.body.type,
-req.body.address,
-    ];
-    db.query (q,[values],(err,data)=>{
-        if (err) return res.json(err)
-        return res.json("salesrep values has been created successfully");
-    })
- })
- app.post ("/category",(req,res)=>{
-   const q= "INSERT INTO category ('catID','Catname')VALUES (?)"
-   const values =["catID from  backend","CatName from backend"]
-   db.query (q,[values],(err,data)=>{
-      if(err) return res.json(err)
-      return res.json("Items created");
+})
+app.get("/salesrep", (req, res) => {
+   const q = "SELECT * FROM sales_rep"
+   db.query(q, (err, data) => {
+      if (err) return res.json(err);
+      return res.json(data);
+
    })
- })
- 
- app.listen( 8800 , ()=>{
-    console.log("Connected to backend !1")
+})
+app.post("/salesrep", (req, res) => {
+   const q = "INSERT INTO sales_rep (RID,NIC,registrationdate,fullname,password,email,phoneNo,type,address) VALUES (?,?,?,?,?,?,?,?,?)"
 
- })
+   const values = [
+      req.body.RID,
+      req.body.NIC,
+      req.body.registrationdate,
+      req.body.fullname,
+      req.body.password,
+      req.body.email,
+      req.body.phoneNo,
+      req.body.type,
+      req.body.address,
+   ];
 
- 
+   // console.log(values);
+   db.query(q, values, (err, data) => {
+      if (err) return res.json(err)
+      return res.json("salesrep values has been created successfully");
+   })
+})
 
- 
- 
+
+app.listen(8800, () => {
+   console.log("Connected to backend !1")
+
+})
+
+
+
+
+
--- Software_project_web_application/zinweb/src/components/adminLogin/AdminLogin.js
+++ Software_project_web_application/zinweb/src/components/adminLogin/AdminLogin.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import classes from './adminLogin.module.css';
+import styles from './AdminLogin.module.css';
 
 
 const AdminLogin = () => {
   return (
-    <div className={classes.main_div}>
-     <div className={classes.div_l}>
-     
-        <div className={classes.image}>  </div>
-   </div>
-
-     <div className={classes.div_r}>  
-     <h1 className={classes.center}>Welcome</h1>
-     <p className={classes.para1}>Please login to Admin Dashboard.</p>
-     <input className={classes.input} type ='text' placeholder='username'></input>
-     <input className={classes.input} type ='password'     placeholder='password'></input>
-     <button className={classes.btn}>Login</button>
-     </div>
-    
+    <div className={styles.main}>
+      <div className={styles.sub_main}>
+        <p>name is java</p>
+      </div>
     </div>
   )
 }
+
+
 export default AdminLogin;
 
 
